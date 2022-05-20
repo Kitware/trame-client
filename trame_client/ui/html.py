f0a7d64d0f86365b47ae7a81a8596eb02a81e59a
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-20 15:09:47 -0600

    docs(api): Add missing information

--- trame_client/ui/html.py
+++ trame_client/ui/html.py
@@ -1,11 +1,20 @@
 from .core import AbstractLayout
 from ..widgets.html import Div
 
+__all__ = [
+    "AbstractLayout",
+    "DivLayout",
+]
 
 class DivLayout(AbstractLayout):
-    """Simple <div> element as a layout"""
-
     def __init__(self, _server, template_name="main", **kwargs):
+        """
+        Simple <div> element as a layout
+
+        :param _server: Server to bound the layout to
+        :param template_name: Name of the template (default: main)
+        :param **kwargs: Optional arguments pass to the <Div> element
+        """
         super().__init__(
             _server, Div(trame_server=_server), template_name=template_name, **kwargs
         )
