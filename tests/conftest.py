a1c9c5c980f5a4b37b2c1b6d024ad39aa493c683
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-06-16 15:41:38 -0600

    fix(testing): Add testing utilities

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,76 +1,25 @@
-import os
-import json
 import pytest
-from xprocess import ProcessStarter
 from pathlib import Path
+from trame_client.utils.testing import FixtureHelper
 
 ROOT_PATH = Path(__file__).parent.parent.absolute()
-
-
-class TrameServerMonitor:
-    def __init__(self, log_path):
-        self._log_path = log_path
-        self._last_state = {}
-        self.port = 0
-        self.update()
-
-    def update(self):
-        last_state_line = "STATE: {}"
-        with open(self._log_path, "r") as f:
-            for line in f.readlines():
-                print(line)
-                if "SERVER_PORT:" in line:
-                    self.port = int(line[13:])
-                if line[:7] == "STATE: ":
-                    last_state_line = line
-
-        self._last_state = json.loads(last_state_line[7:])
-
-    def get_state(self):
-        self.update()
-        return self._last_state
-
-    def get(self, name):
-        self.update()
-        return self._last_state.get(name)
-
-
-def remove_page_urls():
-    BASE_PATH = Path(__file__).parent.with_name("visual_baseline")
-    for root, dirs, files in os.walk(BASE_PATH):
-        if "page_url.txt" in files:
-            full_path = os.path.join(root, "page_url.txt")
-            os.remove(full_path)
-            print(f" - remove: {full_path}")
+HELPER = FixtureHelper(ROOT_PATH)
 
 
 @pytest.fixture()
 def baseline_image():
-    remove_page_urls()
+    HELPER.remove_page_urls()
     yield
-    remove_page_urls()
+    HELPER.remove_page_urls()
 
 
 @pytest.fixture
 def server(xprocess, server_path):
-    name = Path(server_path).name
-
-    class Starter(ProcessStarter):
-        terminate_on_interrupt = True
-        pattern = "App running at:"
-
-        # command to start process
-        args = [
-            "python3",
-            ROOT_PATH / server_path,
-            "--server",
-            "--port",
-            "0",
-        ]
+    name, Starter, Monitor = HELPER.get_xprocess_args(server_path)
 
     # ensure process is running and return its logfile
     logfile = xprocess.ensure(name, Starter)
-    yield TrameServerMonitor(logfile[1])
+    yield Monitor(logfile[1])
 
     # clean up whole process tree afterwards
     xprocess.getinfo(name).terminate()
--- trame_client/utils/testing.py
+++ trame_client/utils/testing.py
@@ -1,4 +1,34 @@
 import json
+from pathlib import Path
+from xprocess import ProcessStarter
+
+
+class TrameServerMonitor:
+    def __init__(self, log_path):
+        self._log_path = log_path
+        self._last_state = {}
+        self.port = 0
+        self.update()
+
+    def update(self):
+        last_state_line = "STATE: {}"
+        with open(self._log_path, "r") as f:
+            for line in f.readlines():
+                print(line)
+                if "SERVER_PORT:" in line:
+                    self.port = int(line[13:])
+                if line[:7] == "STATE: ":
+                    last_state_line = line
+
+        self._last_state = json.loads(last_state_line[7:])
+
+    def get_state(self):
+        self.update()
+        return self._last_state
+
+    def get(self, name):
+        self.update()
+        return self._last_state.get(name)
 
 
 def print_state(**kwargs):
@@ -11,3 +41,30 @@ def enable_testing(server, *state_monitor):
     @server.controller.add("on_server_ready")
     def print_server_port(**kwargs):
         print("SERVER_PORT:", server.port, flush=True)
+
+
+class FixtureHelper:
+    def __init__(self, root_path):
+        self.root_path = Path(root_path)
+
+    def remove_page_urls(self):
+        BASE_PATH = self.root_path / "visual_baseline"
+        for file in BASE_PATH.glob("**/page_url.txt"):
+            file.unlink()
+            print(f" - remove: {file}")
+
+    def get_xprocess_args(self, server_path):
+        class Starter(ProcessStarter):
+            terminate_on_interrupt = True
+            pattern = "App running at:"
+
+            # command to start process
+            args = [
+                "python3",
+                str(self.root_path / server_path),
+                "--server",
+                "--port",
+                "0",
+            ]
+
+        return Path(server_path).name, Starter, TrameServerMonitor
