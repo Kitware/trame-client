67f5e6993bfb3a3974f8dfaefbfed94b5d877942
Kevin Marchais <kevinmarchais@gmail.com>
2024-07-02 17:59:08 +0200

    fix(numpy): remove np.float_ and np.complex_ for compatibility with numpy 2.0

--- trame_client/encoders/numpy.py
+++ trame_client/encoders/numpy.py
@@ -1,4 +1,5 @@
 import json
+
 import numpy as np
 
 
@@ -24,10 +25,10 @@ class NumpyEncoder(json.JSONEncoder):
         ):
             return int(obj)
 
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
 
-        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+        elif isinstance(obj, (np.complex64, np.complex128)):
             return {"real": obj.real, "imag": obj.imag}
 
         elif isinstance(obj, (np.ndarray,)):
